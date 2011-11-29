305f3f0829d0a04c7a6f69a7c811b91b5af24648
Syed <syed1.mushtaq@gmail.com>
2011-11-29 10:36:43 +0530

    Update readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='IPRentalClient',
-    version='0.1',
+    version='0.2',
     author='Syed M Ahmed',
     author_email='syed1.mushtaq@gmail.com',
     packages=['iprental'],
